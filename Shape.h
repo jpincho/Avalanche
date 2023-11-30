d65eb472f9d211461f2f07bb9c5488390ebc3877
João Tiago Pincho <joao.tiago.pincho@gmail.com>
2023-11-30 22:59:36 +0000

    added int variant of point

--- Shape.h
+++ Shape.h
@@ -11,31 +11,42 @@
 #include "Framework.h"
 #include <vector>
 
-struct CPoint2d
+template <typename Type> struct TPoint2d
 {
-	CPoint2d() = default;
-	CPoint2d(float x, float y) : X(x), Y(y) {}
+	TPoint2d() = default;
+	TPoint2d(Type x, Type y) : X(x), Y(y) {}
 
-	CPoint2d &operator += (const CPoint2d &Other)
+	TPoint2d<Type> &operator += (const TPoint2d<Type> &Other)
 	{
 		X += Other.X;
 		Y += Other.Y;
 		return *this;
 	}
-	CPoint2d &operator -= (const CPoint2d &Other)
+	TPoint2d<Type> &operator -= (const TPoint2d<Type> &Other)
 	{
 		X -= Other.X;
 		Y -= Other.Y;
 		return *this;
 	}
-	CPoint2d &operator /= (const float Scalar)
+	TPoint2d<Type> &operator /= (const float Scalar)
 	{
 		X /= Scalar;
 		Y /= Scalar;
 		return *this;
 	}
-	float X, Y;
+	bool operator == (const TPoint2d<Type> &Other) const
+	{
+		return ((X == Other.X) && (Y == Other.Y));
+	}
+	bool operator != (const TPoint2d<Type> &Other) const
+	{
+		return !operator ==(Other);
+	}
+	Type X, Y;
 };
+
+typedef TPoint2d <float> CPoint2d;
+typedef TPoint2d <int> CPoint2di;
 
 class CShape
 {
